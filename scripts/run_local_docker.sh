46e009061d9c4716c3cb33409a553f5c2705a4f3
mihal277 <mihal277@gmail.com>
2021-12-12 22:32:40 +0200

    Limit max workers for local docker deployment to 2
    By default Docker desktop limits memory of a container to 2gb,
    currently cunning more workers causes constant ooms.

--- scripts/run_local_docker.sh
+++ scripts/run_local_docker.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 DEFAULTPORT=8081
+MAX_WORKERS=2
 PORT="${1:-$DEFAULTPORT}"
 
 (docker stop smart_word_hints || true) && \
 docker build -t smart_word_hints smart_word_hints_api/ && \
-docker run --name smart_word_hints -p $PORT:8081 --rm -d smart_word_hints
+docker run --name smart_word_hints -e MAX_WORKERS="$MAX_WORKERS" -p $PORT:8081 --rm -d smart_word_hints
 
 echo "API available at http://localhost:$PORT/"
