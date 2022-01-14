9d590ba1a33c118027fe969a24d3a295e747b418
mihal277 <mihal277@gmail.com>
2022-01-14 18:53:24 +0200

    Bump nltk to >=3.6.6

--- scripts/compile_requirements.sh
+++ scripts/compile_requirements.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-cd smart_word_hints_api/requirements && \
+(cd smart_word_hints_api/requirements && \
 pip-compile && \
-pip-compile dev_requirements.in
+pip-compile dev_requirements.in)
+
+(cd scripts/assets_postprocessing && pip-compile)
+
+(cd scripts/scrapers && pip-compile)
