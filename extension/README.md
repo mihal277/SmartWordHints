cf83294e3bd4d3ccb7bbd26de17f53af7fec0ce3
mihal277 <mihal277@gmail.com>
2022-10-16 13:35:46 +0200

    Use Mozilla's Readability for content extraction
    Add webpack

---
+++ extension/README.md
@@ -0,0 +1,13 @@
+# Extension
+
+### Running in a browser
+
+```
+yarn install
+yarn start
+```
+
+In another terminal 
+```
+yarn run-in-browser
+```
--- extension/tests/sum.test.js
+++
@@ -1,7 +0,0 @@
-import sum from '../scripts/get-article-nodes';
-
-describe('Function sum', () => {
-  test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
-  });
-});
