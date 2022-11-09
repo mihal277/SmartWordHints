a1863f9fa219247a7d86e730a311b5aa745283e7
mihal277 <mihal277@gmail.com>
2022-11-09 08:37:14 +0100

    Modify extension to show overlay with extracted article

--- extension/src/scripts/background.js
+++ extension/src/scripts/background.js
@@ -1,4 +1,4 @@
-const DEFAULT_API_URL = 'http://localhost/api/get_hints';
+import { DEFAULT_API_URL, EXTENSION_ICON_CLICKED_COMMAND } from './constants';
 
 function setDefaultURL() {
   browser.storage.sync.set({ api_url: DEFAULT_API_URL });
@@ -10,27 +10,35 @@ function initOptions() {
     (results) => (!('api_url' in results) ? setDefaultURL() : {}),
   );
 }
+initOptions();
 
-function handleMessage(request, sender, sendResponse) {
-  if (request.query === 'get_hints') {
-    const payload = { text: request.messsage, options: { difficulty: 2000 } };
-    const url = browser.storage.sync.get('api_url');
-    url.then((res) => {
-      fetch(res.api_url, {
-        method: 'POST',
-        body: JSON.stringify(payload),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-        .then((response) => response.json())
-        .then((json) => sendResponse(json));
-      // .catch(error => ...)
-    });
-    return true;
-  }
-  return false;
+// function handleMessage(request, sender, sendResponse) {
+//   if (request.query === 'get_hints') {
+//     const payload = { text: request.messsage, options: { difficulty: 2000 } };
+//     const url = browser.storage.sync.get('api_url');
+//     url.then((res) => {
+//       fetch(res.api_url, {
+//         method: 'POST',
+//         body: JSON.stringify(payload),
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//       })
+//         .then((response) => response.json())
+//         .then((json) => sendResponse(json));
+//       // .catch(error => ...)
+//     });
+//     return true;
+//   }
+//   return false;
+// }
+//
+// browser.runtime.onMessage.addListener(handleMessage);
+
+function startSmartWordHints(tab) {
+  browser.tabs.sendMessage(tab.id, { command: EXTENSION_ICON_CLICKED_COMMAND }).catch((error) => {
+    console.error(`Error starting smart word hints: ${error}`);
+  });
 }
 
-initOptions();
-browser.runtime.onMessage.addListener(handleMessage);
+browser.browserAction.onClicked.addListener(startSmartWordHints);
